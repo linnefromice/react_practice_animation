ba4b2b8f696f7943f6edbb35bb3a23caa493dc34
linnefromice <linnefromice@gmail.com>
2020-05-14 01:25:45 +0900

    create sample svg

--- pj-react-spring/src/App.js
+++ pj-react-spring/src/App.js
@@ -32,22 +32,6 @@ function SampleBackGroundOne () {
   );
 }
 
-function SampleSvgOne() {
-  return (
-    <div style={{width: 500, height: 200, background: 'linear-gradient(to bottom right, #de350b, #0065ff)'}}>
-      <svg mlns='http://www.w3.org/2000/svg' viewBox="0 0 64 64">
-        <defs>
-          <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="0%" stopColor="#ad3" />
-            <stop offset="50%" stopColor="#add" />
-          </linearGradient>
-        </defs>
-        <path d='M0 10 C30 28 38 0 64 10 L64 0 L0 0 Z' fill="url(#grad)"/>
-      </svg>
-    </div>
-  )
-}
-
 function App() {
   return (
     <GradientOne/>
