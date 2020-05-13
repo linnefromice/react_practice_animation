07c3c2a3562b21ecc24a8eaa6b20018dd3c4fc76
linnefromice <linnefromice@gmail.com>
2020-05-13 18:39:29 +0900

    add svg sample

--- pj-react-spring/src/App.js
+++ pj-react-spring/src/App.js
@@ -15,7 +15,7 @@ const Container = Keyframes.Spring(async next => {
   }
 })
 
-function App() {
+function SampleBackGroundOne () {
   return (
     <Container
       reset
@@ -28,6 +28,28 @@ function App() {
         </div>
       )}
     </Container>
+  );
+}
+
+function SampleSvgOne() {
+  return (
+    <div style={{width: 500, height: 200, background: 'linear-gradient(to bottom right, #de350b, #0065ff)'}}>
+      <svg mlns='http://www.w3.org/2000/svg' viewBox="0 0 64 64">
+        <defs>
+          <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="100%">
+            <stop offset="0%" stopColor="#ad3" />
+            <stop offset="50%" stopColor="#add" />
+          </linearGradient>
+        </defs>
+        <path d='M0 10 C30 28 38 0 64 10 L64 0 L0 0 Z' fill="url(#grad)"/>
+      </svg>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <SampleSvgOne/>
   );
 }
 
